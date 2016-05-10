5b135b20bf5db9fea68d564730679863fddb05db
Felix Möller <git@felix-moeller.net>
2016-05-10 21:29:56 +0200

    Update README.md and implement UDP server

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # network-slave
 Simple server mapping network requests to local commands
+
+## Usage
+To start the server, simply invoke it by using:
+```bash
+network-slave [PATH-TO-CONFIG-FILE]
+```
+
+## Configuration
+You can define HTTP and UDP server settings in the config file like so:
+```ini
+[host]
+bind=127.0.0.1
+http=23000
+udp=23001
+
+[commands]
+play/$id = /opt/play.sh $id
+```
+
+## HTTP
+To connect to the HTTP server, simply send an HTTP 1.1 request:
+```bash
+curl http://$MACHINE:$PORT/$COMMAND/$PARAMS
+```
+
+## UDP
+To connect to the UDP server, simply send a datagram like so:
+```bash
+echo -n "$COMMAND/$PARAMS" | ncat -u "$HOST" "$PORT"
+```
--- network-slave.py
+++ network-slave.py
@@ -13,14 +13,14 @@ from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from time import sleep
 
-host      = '127.0.0.1'
-port_http = 23000
-port_udp  = 23001
+config = configparser.ConfigParser()
 
-red = '\033[1;31m'
-blue = '\033[1;34m'
+# Escape codes
+red   = '\033[1;31m'
+blue  = '\033[1;34m'
 green = '\033[1;32m'
-nc = '\033[1;m'
+nc    = '\033[1;m'
+begin = '\033[80D'
 
 
 class httpRequestHandler(BaseHTTPRequestHandler):
@@ -30,38 +30,55 @@ class httpRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         message = execute(self.path, 'HTTP', self.client_address[0])
+        if not message:
+            return
         self.wfile.write(message)
         return
 
     def log_request(handler, code):
         return
 
+    def log_message(handler, fmt, code, error):
+        print('  [ %sHTTP%s ] %sERROR %s%s: %s' % (blue, nc, red, int(code), nc, error))
+        return
+
 
 def serve_udp():
-    print('Starting %sUDP%s  server on %s:%s ... ' % (green, nc, host, port_udp), end="")
+    print('Starting %sUDP%s  server on %s:%s ... ' % (green, nc, config['host']['bind'], config['ports']['udp']), end="")
     # Create UDP socket
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind((host, port_udp))
+        #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((config['host']['bind'], int(config['ports']['udp'])))
     except socket.error:
         print ('Failed to create UDP socket')
         sys.exit(1)
     print('Ready!')
 
     while True:
+        #(cs, addr) = s.accept()
+        #data = cs.recv(1024).decode()
         data, addr = s.recvfrom(1024)
         message = execute(data.decode('utf-8'), 'UDP', addr[0])
-        print(message)
+        if not message:
+            continue
+        #print(dir(s))
+        #print(dir(s.send))
+        #print(addr)
+        #print(type(addr[0]))
+        #s.send(message, int(addr[0]))
+        s.sendto(message, addr)
+        #print(message)
 
 
 def serve_http():
-    print('Starting %sHTTP%s server on %s:%s ... ' % (blue, nc, host, port_http), end="")
-    httpd = HTTPServer((host, port_http), httpRequestHandler)
+    print('Starting %sHTTP%s server on %s:%s ... ' % (blue, nc, config['host']['bind'], config['ports']['http']), end="")
+    httpd = HTTPServer((config['host']['bind'], int(config['ports']['http'])), httpRequestHandler)
     print('Ready!')
     httpd.serve_forever()
 
 
-def parse_cmd(path, split_char='/', split_shift=0, return_string=False):
+def parse_cmd(path, split_char='/', split_shift=0):
     ps=path.split(split_char)
 
     function = check_function(ps[split_shift])
@@ -82,18 +99,27 @@ def check_function(function):
 
 
 def signal_handler(signal, frame):
-    print('Stopping program...')
+    # Need to move cursor to beginning to omit '^C'
+    print('%sStopping program...' % begin)
     #sys.exit(0)
     os._exit(0)
 
 
 def execute(cmd, proto, addr):
+    # TODO Check cmd
+    #print(list(config['commands'].items()))
+    #for i in config['commands'].items():
+    #    print(i)
+
     if proto == "UDP":
         protoc = green
         pcmd = parse_cmd(cmd, split_char=';')
     else:
         protoc = blue
         pcmd = parse_cmd(cmd, split_shift=1)
+
+    if not pcmd:
+        return False
 
     pret = ''
     for p in pcmd[1:]:
@@ -109,6 +135,7 @@ def execute(cmd, proto, addr):
 
     ret = "ERROR"
     error = None
+    subret = None
     try:
         subret = subprocess.run(pcmd, stdout=subprocess.PIPE, check=True)
     except subprocess.CalledProcessError as e:
@@ -119,10 +146,23 @@ def execute(cmd, proto, addr):
     if error is not None:
         print('  [ %s%4s%s ] %sERROR%s: %s' % (protoc, proto, nc, red, nc, error))
 
-    return subret.stdout
+    if subret is not None:
+        return subret.stdout
+    else:
+        return False
 
 
 def main():
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+    else:
+        config_file = '~/.config/network-slave/config'
+
+    if not os.path.exists(config_file):
+        sys.exit('%sERROR%s: Config file not found "%s"' % (red, nc, config_file))
+
+    config.read(config_file)
+
     thread_http = threading.Thread(target=serve_http)
     thread_http.start()
     sleep(.1)
@@ -133,7 +173,7 @@ def main():
 
     # Catch Ctrl+C
     signal.signal(signal.SIGINT, signal_handler)
-    print('Press Ctrl+C to end')
+    print('Press Ctrl+C to end\n')
     signal.pause()
 
 
